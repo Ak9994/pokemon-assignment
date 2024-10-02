0c955aefc8aa08904d39289a833fe75cc26e370e
Ak9994 <163250688+Ak9994@users.noreply.github.com>
2024-10-02 08:53:36 +0530

    Add files via upload

---
+++ pokemon/main.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    string player_name;
+
+    cout << "enter name";
+    cin >> player_name;
+
+    cout << "adventure is starting now ," << player_name << endl;
+
+    return 0;
+}
