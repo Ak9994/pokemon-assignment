af59213aadb4bc18417d051051f0f0cc8e2b6d9c
Ak9994 <163250688+Ak9994@users.noreply.github.com>
2024-10-26 14:12:26 +0530

    Update main.cpp

--- pokemon/main.cpp
+++ pokemon/main.cpp
@@ -135,7 +135,7 @@ class professorOak{
 
         void offerchoice(player &player){
         //select
-            cout << player_name << ": You must be eager to start your adventure. But first, you’ll need a Pokemon of your own!" << endl;
+            cout << player.name << ": You must be eager to start your adventure. But first, you’ll need a Pokemon of your own!" << endl;
 		    waitforinput();
             cout << "Treecko, is a grass type pokemon! easy choice as a starter" << endl;
             waitforinput();
@@ -152,7 +152,36 @@ class professorOak{
             waitforinput();
 
         }
+
+        
 };
+
+void gameloop(player &Player){
+            int choice;
+            bool keepPlaying = true;
+            while(keepPlaying){
+                cout << "\n What would you like to do next, " << Player.name << "?\n";
+	            cout << "1. Battle Wild Pokémon\n";
+	            cout << "2. Visit PokeCenter\n";
+	            cout << "3. Challenge Gyms\n";
+	            cout << "4. Enter Pokémon League\n";
+	            cout << "5. Quit\n";
+	            cout << "Enter your choice: ";
+	            cin >> choice;
+
+                switch(choice){
+                    case 1: { cout << "You look around... but all the wild Pokemon are on vacation. Maybe try again later? \n"; break; }
+                    case 2: { cout << "You head to the PokeCenter, but Nurse Joy is out on a coffee break. Guess your Pokemon will have to tough it out for now! \n"; break; }
+                    case 3: { cout << "You march up to the Gym, but it's closed for renovations. Seems like even Gym Leaders need a break! \n"; break; }
+                    case 4: { cout << "You boldly step towards the Pokemon League... but the gatekeeper laughs and says, 'Maybe next time, champ!' \n"; break; }
+                    case 5: { cout << "You try to quit, but Professor Oak's voice echoes: 'There's no quitting in Pokemon training!' \n"; break; }
+                    default: { cout << "Please select right choice"; break; }
+                }
+                waitforinput();
+                //exit loop
+                cout << "saving game now , Please dont turn off the power" << Player.name << "Cya around :) " << "\n" ;
+            }
+        }
 
 int main(){
 
@@ -172,6 +201,7 @@ int main(){
     prof.explainquest(Player);
 
     cout << "This Adventure is starting now" << endl;
+    gameloop(Player);
     
     
     return 0;
