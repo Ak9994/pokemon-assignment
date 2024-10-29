1ec91189994c37ce6d46f5d3ce438ae0f9bb446a
Ak9994 <163250688+Ak9994@users.noreply.github.com>
2024-10-29 19:22:02 +0530

    Update main.cpp

--- pokemon/main.cpp
+++ pokemon/main.cpp
@@ -1,13 +1,24 @@
-#include "utility.hpp"
-#include "pokemontype.hpp"
-#include "pokemonchoice.hpp"
-#include "pokemon.hpp"
+#include "game.hpp"
 #include "player.hpp"
+#include "pokemonchoice.hpp"
+#include "pokemontype.hpp"
 #include "oak.hpp"
+#include "utility.hpp"
 
 #include <iostream>
 #include <string>
 
 using namespace std;
 
-int main(){}
+int main(){
+    professorOak professor("Professor Oak");
+    player Player;
+
+    professor.Oak_intro(Player);
+    professor.offerchoice(Player);
+    professor.explainquest(Player);
+
+    game Game;
+    Game.gameloop(Player);
+
+}
