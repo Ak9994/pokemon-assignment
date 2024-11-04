e6c5eabd23b7d4bedf965e16034e0c3da265148a
Ak9994 <163250688+Ak9994@users.noreply.github.com>
2024-11-04 20:34:33 +0530

    Update pokemon.cpp

--- pokemon/pokemon.cpp
+++ pokemon/pokemon.cpp
@@ -18,6 +18,39 @@ pokemon::pokemon(const pokemon &p_ref){
 
 pokemon::~pokemon(){}
 
-void pokemon::attack() {
-    cout << name << " attacks with a powerful move!\n";
+void pokemon::attack(pokemon &target) {
+    
+    int atkdamage;
+
+    cout << name << " attacks with a move! , which dealt:" << atkdamage << "dmg\n" ;
+
+    target.takedamage(atkdamage);
+
+}
+
+void pokemon::takedamage(int dmg){
+    
+    health -= dmg;
+    
+    if(health < 0){  health = 0; }
+
+}
+
+bool pokemon::isfaint(){
+    if(health <= 0) return true;
+}
+
+void pokemon::battleloop(pokemon &trainer_pokemon,pokemon &wild_pokemon){
+    while(!trainer_pokemon.isfaint() && !wild_pokemon.isfaint()){
+        trainer_pokemon.attack(wild_pokemon);
+        if(!wild_pokemon.isfaint()){
+            wild_pokemon.attack(trainer_pokemon);
+        }
+    }
+
+    if(trainer_pokemon.isfaint()){
+        cout << trainer_pokemon.name << "has fainted !!! \n";
+    } else {
+        cout << wild_pokemon.name << "has fainted. \n";
+    }
 }
